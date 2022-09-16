dad7438519aea7329fe8070b8d798517572c3a23
angelicamp <enfermagemangelicamp@gmail.com>
2022-09-16 13:17:11 -0300

    recebendo mensagens do cliente

--- app.js
+++ app.js
@@ -24,3 +24,23 @@ function resposta (req, res) {
          }
      );
 }
+io.on("connection", function(socket){
+    socket.on("enviar mensagem", function(mensagem_enviada, callback){
+        mensagem_enviada = "[ " + pegarDataAtual() + " ]: " + mensagem_enviada;
+
+        io.sockets.emit("atualizar mensagens", mensagem_enviada);
+        callback();
+    });
+});
+function pegarDataAtual(){
+ var dataAtual = new Date();
+ var dia = (dataAtual.getDate()<10 ? '0' : '') + dataAtual.getDate();
+ var mes = ((dataAtual.getMonth() + 1)<10 ? '0' : '') + (dataAtual.getMonth() + 1);
+ var ano = dataAtual.getFullYear();
+ var hora = (dataAtual.getHours()<10 ? '0' : '') + dataAtual.getHours();
+ var minuto = (dataAtual.getMinutes()<10 ? '0' : '') + dataAtual.getMinutes();
+ var segundo = (dataAtual.getSeconds()<10 ? '0' : '') + dataAtual.getSeconds();
+
+ var dataFormatada = dia + "/" + mes + "/" + ano + " " + hora + ":" + minuto + ":" + segundo;
+ return dataFormatada;
+}
